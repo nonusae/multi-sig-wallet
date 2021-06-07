581bdb9e8b6e66918f64bc7e540a0bc28f016427
nonusae <nonusae@gmail.com>
2021-06-07 17:01:38 +0700

    Add more test to project

--- test/wallet.js
+++ test/wallet.js
@@ -1,3 +1,5 @@
+const { expectRevert } = require('@openzeppelin/test-helpers');
+
 const Wallet = artifacts.require('Wallet');
 
 contract('Wallet', (accounts) => {
@@ -34,5 +36,43 @@ contract('Wallet', (accounts) => {
     assert(transfers[0].to === accounts[5]);
     assert(transfers[0].approvals === '0');
     assert(transfers[0].sent === false);
+  });
+
+  it('should NOT create transfer if not approved', async () => {
+    await expectRevert(
+      wallet.createTransfer(100, accounts[5], { from: accounts[4] }),
+      'only approver allowed',
+    );
+  });
+
+  it('should increment approval', async () => {
+    await wallet.createTransfer(100, accounts[5], {
+      from: accounts[0],
+    });
+
+    await wallet.approveTransfer(0, { from: accounts[0] });
+
+    const transfers = await wallet.getTransfers();
+    const balance = await web3.eth.getBalance(wallet.address);
+    assert(transfers[0].approvals === '1');
+    assert(transfers[0].sent === false);
+    assert(balance === '1000');
+  });
+
+  it('should sent transfer if quarom reached', async () => {
+    const balanceBefore = web3.utils.toBN(
+      await web3.eth.getBalance(accounts[6]),
+    );
+    await wallet.createTransfer(100, accounts[6], {
+      from: accounts[0],
+    });
+
+    await wallet.approveTransfer(0, { from: accounts[0] });
+    await wallet.approveTransfer(0, { from: accounts[1] });
+
+    const balanceAfter = web3.utils.toBN(
+      await web3.eth.getBalance(accounts[6]),
+    );
+    assert(balanceAfter.sub(balanceBefore).toNumber() === 100);
   });
 });
