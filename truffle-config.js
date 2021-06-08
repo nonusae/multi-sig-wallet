a5e9043c59f2ae9408de01278d8db22ac22301c6
nonusae <nonusae@gmail.com>
2021-06-08 14:19:36 +0700

    Add header component for display approvers and quorum

--- truffle-config.js
+++ truffle-config.js
@@ -54,7 +54,7 @@ module.exports = {
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
-    network_id: 5777, // Custom network
+    // network_id: 5777, // Custom network
     // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
     // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
     // from: <address>,        // Account to send txs from (default: accounts[0])
