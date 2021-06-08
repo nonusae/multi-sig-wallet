837190ded5745965988026026687659e3df35206
nonusae <nonusae@gmail.com>
2021-06-08 11:09:28 +0700

    Fix typo and truffle-config file

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3';
-import Wallet from '/contract/Wallet.json';
+import Wallet from './contracts/Wallet.json';
 
 const getWeb3 = () => {
   return new Web3('http://localhost:9545');
@@ -8,6 +8,7 @@ const getWeb3 = () => {
 const getWallet = async (web3) => {
   const networkId = await web3.eth.net.getId();
   const contractDeployment = Wallet.networks[networkId];
+
   return new web3.eth.Contract(
     Wallet.abi,
     contractDeployment && contractDeployment.address,
